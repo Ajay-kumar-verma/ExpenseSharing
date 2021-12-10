f875d2aefcd07e4ec1a8bc27927418cd593b3ba0
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2021-12-10 15:27:53 +0530

    Expense Sharing

---
+++ Main.java
@@ -0,0 +1,20 @@
+// EXPENSE SHARING AMONG GROUP 
+// EXPESE DETAILS AND SHARING 
+
+
+public class Main 
+{
+public static void main(String args[]){
+for (String str : args) { // it wont execute 
+    System.out.println(str);
+}
+// System.out.println(args[0]);
+// Error
+
+String a[];
+ a=new String[0];
+ System.out.println(a[0]);
+
+ 
+}
+}
